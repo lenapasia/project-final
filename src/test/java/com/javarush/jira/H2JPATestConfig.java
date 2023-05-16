624da4526d7051b56f2d08227fc221cd98537164
lenapasia <104163952+lenapasia@users.noreply.github.com>
2023-05-16 21:50:44 +0300

    test(bugtracking): delete obsolete PostgreSQL config
    task #4

--- src/test/java/com/javarush/jira/H2JPATestConfig.java
+++ src/test/java/com/javarush/jira/H2JPATestConfig.java
@@ -7,14 +7,12 @@ import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
-@Profile("!" + ProfileNames.TEST_POSTGRESQL)
 @Configuration
 public class H2JPATestConfig {
 
+    @Profile("!" + ProfileNames.TEST_POSTGRESQL)
     @Bean
     public DataSource getDataSource() {
-        System.out.println("--- !!! Test with H2 database!!! ---");
-
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.h2.Driver");
         dataSourceBuilder.url("jdbc:h2:mem:test");
