b83afac6be6942c8075fa267d4b397089392e397
lenapasia <104163952+lenapasia@users.noreply.github.com>
2023-05-17 13:17:42 +0300

    fix(bugtracking): add length validation for task's tag
    task #6

--- src/main/java/com/javarush/jira/bugtracking/TaskService.java
+++ src/main/java/com/javarush/jira/bugtracking/TaskService.java
@@ -4,11 +4,14 @@ import com.javarush.jira.bugtracking.internal.mapper.TaskMapper;
 import com.javarush.jira.bugtracking.internal.model.Task;
 import com.javarush.jira.bugtracking.internal.repository.TaskRepository;
 import com.javarush.jira.bugtracking.to.TaskTo;
+import jakarta.validation.constraints.Size;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.List;
 import java.util.Set;
 
+@Validated
 @Service
 public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository> {
     public TaskService(TaskRepository repository, TaskMapper mapper) {
@@ -19,7 +22,7 @@ public class TaskService extends BugtrackingService<Task, TaskTo, TaskRepository
         return mapper.toToList(repository.getAll());
     }
 
-    public void updateTags(long taskId, Set<String> tags) {
+    public void updateTags(long taskId, Set<@Size(min = 2, max = 32) String> tags) {
         Task task = repository.getExisted(taskId);
         task.getTags().clear();
         if (tags != null) {
--- src/test/java/com/javarush/jira/bugtracking/TaskTagControllerTest.java
+++ src/test/java/com/javarush/jira/bugtracking/TaskTagControllerTest.java
@@ -18,9 +18,9 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static com.javarush.jira.bugtracking.TaskTagController.REST_URL;
+import static com.javarush.jira.bugtracking.TaskTestData.TASK_ID;
 import static com.javarush.jira.common.util.JsonUtil.writeValue;
 import static com.javarush.jira.login.internal.web.UserTestData.USER_MAIL;
-import static com.javarush.jira.bugtracking.TaskTestData.TASK_ID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,14 +31,14 @@ class TaskTagControllerTest extends AbstractControllerTest {
     TaskRepository repository;
 
     @Test
-    void updateUnauthorized() throws Exception {
+    void update_shouldThrowUnauthorizedException_whenUserIsNotAuthorized() throws Exception {
         perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", TASK_ID))
                 .andExpect(status().isUnauthorized());
     }
 
     @Test
     @WithUserDetails(value = USER_MAIL, setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void update() throws Exception {
+    void update_shouldStoreTags() throws Exception {
         final Set<String> newTags = Stream.of("tag1", "tag2").collect(Collectors.toCollection(HashSet::new));
 
         perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", TASK_ID)
@@ -54,7 +54,7 @@ class TaskTagControllerTest extends AbstractControllerTest {
 
     @Test
     @WithUserDetails(value = USER_MAIL, setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void updateClearTags() throws Exception {
+    void update_shouldClearTags_whenGivenEmpty() throws Exception {
         // set new tags
         final Set<String> newTags = Stream.of("tag1", "tag2").collect(Collectors.toCollection(HashSet::new));
         perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", TASK_ID)
@@ -81,7 +81,7 @@ class TaskTagControllerTest extends AbstractControllerTest {
 
     @Test
     @WithUserDetails(value = USER_MAIL, setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void updateDuplicateTags() throws Exception {
+    void update_shouldStoreOnlyUniqueTags_whenGivenDuplicates() throws Exception {
         final List<String> newTags = Arrays.asList("tag1", "tag2", "tag1");
 
         perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", TASK_ID)
@@ -90,15 +90,14 @@ class TaskTagControllerTest extends AbstractControllerTest {
                 .andDo(print())
                 .andExpect(status().isNoContent());
 
-        // check that tags were saved into database without duplicates
         final Set<String> expectedTags = new HashSet<>(Arrays.asList("tag1", "tag2"));
         final Task task = repository.getExisted(TASK_ID);
         assertThat(task.getTags()).usingRecursiveComparison().isEqualTo(expectedTags);
     }
 
     @Test
-    @WithUserDetails(value = USER_MAIL, setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void updateMissingBody() throws Exception {
+    @WithUserDetails(value = USER_MAIL)
+    void update_shouldThrowException_whenRequestBodyIsMissing() throws Exception {
         perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", -1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(writeValue(null)))
@@ -107,12 +106,23 @@ class TaskTagControllerTest extends AbstractControllerTest {
     }
 
     @Test
-    @WithUserDetails(value = USER_MAIL, setupBefore = TestExecutionEvent.TEST_EXECUTION)
-    void updateTaskNotFound() throws Exception {
+    @WithUserDetails(value = USER_MAIL)
+    void update_shouldThrowNotFoundException_whenTaskDoesNotExist() throws Exception {
         perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", -1)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(writeValue(new HashSet<>())))
                 .andDo(print())
                 .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @WithUserDetails(value = USER_MAIL)
+    void update_shouldThrowException_whenGivenTagIsInvalid() throws Exception {
+        final Set<String> invalidTags = Set.of("t", "tag2");
+        perform(MockMvcRequestBuilders.put(REST_URL + "/{id}/tags", TASK_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(writeValue(invalidTags)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
     }
 }
